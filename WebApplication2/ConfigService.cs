eebf1fd8e0d9aaf945df545c1418a846b2011e30
Dan Da'neshgar <dan@lastsignal.com>
2016-03-17 09:55:09 -0500

    Cleanup unnecessary Auth components from the newly made project from template and add hooks to the existing sso application

---
+++ WebApplication2/ConfigService.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using Microsoft.Owin;
+
+namespace WebApplication2
+{
+    public class ConfigService
+    {
+        public class Account
+        {
+            private static string AuthenticatorBasePass
+            {
+                get { return ConfigurationManager.AppSettings.Get("authenticator-base-url"); }
+            }
+
+            public static readonly string LoginUrl = AuthenticatorBasePass + "Account/Login";
+            public static readonly string RegisterUrl = AuthenticatorBasePass + "Account/Register";
+            public static readonly string ManageUrl = AuthenticatorBasePass + "Manage";
+            public static readonly string LogOffUrl = AuthenticatorBasePass + "Account/LogOff";
+
+            public static string GetLoginWithReturnUrl(string returnUrl)
+            {
+                return LoginUrl + new QueryString("returnUrl", returnUrl);
+            }
+        }
+    }
+}
